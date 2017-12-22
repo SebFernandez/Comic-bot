4c5f3b8f8d37757cf1dc3ec8ca2f7be37560463d
zeby95 <seba.fernandez95@gmail.com>
2017-12-22 17:26:45 -0300

    Fix

--- comic_bot.py
+++ comic_bot.py
@@ -49,55 +49,61 @@ def write (comicStrip):
 		print (error + str (e)) 	
 
 def comicTweet (comicStrip):													#Here decides which comic strip will tweet
-	if (comicStrip == 0):
-		URL = requests.get (snoopy.url)
+	try:
+		if (comicStrip == 0):
+			URL = requests.get (snoopy.url)
 
-		soup = BeautifulSoup (URL.content, 'html.parser')
-		img = soup.find_all ('picture')
-		auxString = img [1]
-		auxString = auxString.find_all ('img')
-		snoopy.url = auxString [0].get ('src')
+			soup = BeautifulSoup (URL.content, 'html.parser')
+			img = soup.find_all ('picture')
+			auxString = img [1]
+			auxString = auxString.find_all ('img')
+			snoopy.url = auxString [0].get ('src')
 
-		auxString = soup.find_all (class_="btn-calendar-nav item-control gc-calendar-wrapper js-calendar-wrapper")
-		auxString = auxString [0].get ('data-date')
-		YY = auxString [0:4]
-		MM = auxString [5:7]
-		DD = auxString [8:11]
-		snoopy.date = DD + '/' + MM + '/' + YY
+			auxString = soup.find_all (class_="btn-calendar-nav item-control gc-calendar-wrapper js-calendar-wrapper")
+			auxString = auxString [0].get ('data-date')
+			YY = auxString [0:4]
+			MM = auxString [5:7]
+			DD = auxString [8:11]
+			snoopy.date = DD + '/' + MM + '/' + YY
 
-	elif (comicStrip == 1 ):
-		YY = random.randrange (2000, time.localtime(time.time()).tm_year) 						#To have the years up to date.
-		MM = random.randrange (1,12)
-		if (MM % 2 == 0):
-			DD = random.randrange (1, 30)
-		else:
-			DD = random.randrange (1,31)
+		elif (comicStrip == 1 ):
+			YY = random.randrange (2000, time.localtime(time.time()).tm_year) 						#To have the years up to date.
+			MM = random.randrange (1,12)
+			if (MM % 2 == 0):
+				DD = random.randrange (1, 30)
+			else:
+				DD = random.randrange (1,31)
 
-		garfield.date = str (DD) + '/' + str (MM) + '/' + str (YY)
+			garfield.date = str (DD) + '/' + str (MM) + '/' + str (YY)
 
-		if (DD < 10 and MM < 10):													#Format to access at the URL picture.
-			garfield.url = garfield.url + str (YY) + '/' + str (YY) + '-0' + str (MM) + '-0' + str (DD) + '.gif'
-		elif (DD < 10):
-			garfield.url = garfield.url + str (YY) + '/' + str (YY) + '-' + str (MM) + '-0' + str (DD) + '.gif'
-		elif (MM < 10):
-			garfield.url = garfield.url + str (YY) + '/' + str (YY) + '-0' + str (MM) + '-' + str (DD) + '.gif'
-		else:
-			garfield.url = garfield.url + str (YY) + '/' + str (YY) + str (MM) + str (DD) + '.gif'
+			if (DD < 10 and MM < 10):													#Format to access at the URL picture.
+				garfield.url = garfield.url + str (YY) + '/' + str (YY) + '-0' + str (MM) + '-0' + str (DD) + '.gif'
+			elif (DD < 10):
+				garfield.url = garfield.url + str (YY) + '/' + str (YY) + '-' + str (MM) + '-0' + str (DD) + '.gif'
+			elif (MM < 10):
+				garfield.url = garfield.url + str (YY) + '/' + str (YY) + '-0' + str (MM) + '-' + str (DD) + '.gif'
+			else:
+				garfield.url = garfield.url + str (YY) + '/' + str (YY) + str (MM) + str (DD) + '.gif'
 
-	elif (comicStrip == 2):
-		URL = requests.get (cyanide.url)
+		elif (comicStrip == 2):
+			URL = requests.get (cyanide.url)
 
-		soup = BeautifulSoup (URL.content, 'html.parser')
-		img = soup.find_all (id = 'main-comic')
-		cyanide.url = 'http:' + img[0].get ('src')
-		
-		auxString = soup.find_all (class_ = 'author-credit-name')
-		auxString = auxString [0].get_text ()
-		cyanide.comicAuthor = auxString [3: len (auxString)]
+			soup = BeautifulSoup (URL.content, 'html.parser')
+			img = soup.find_all (id = 'main-comic')
+			cyanide.url = 'http:' + img[0].get ('src')
+			
+			auxString = soup.find_all (class_ = 'author-credit-name')
+			auxString = auxString [0].get_text ()
+			cyanide.comicAuthor = auxString [3: len (auxString)]
 
-		auxString = soup.find_all (class_ = 'zeta small-bottom-margin past-week-comic-title')
-		cyanide.date = auxString [0].get_text ()
-		cyanide.date = cyanide.date.replace ('.', '/')
+			auxString = soup.find_all (class_ = 'zeta small-bottom-margin past-week-comic-title')
+			cyanide.date = auxString [0].get_text ()
+			cyanide.date = cyanide.date.replace ('.', '/')
+	except exception as e:
+		print ('\n\t\t-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-\n')
+		print (">> ERROR!\n>>" + e.reason)
+		print ('\n\t\t-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-\n')
+		pass
 
 def log (comicStrip):
 	print (">> Comic: \t" + comicArray [comicStrip].comicName)
@@ -108,7 +114,7 @@ def log (comicStrip):
 	print (">> Tweet!\n\t---------------------------------------------------------------")
 
 def upload ():
-	bot = botLoggin ()
+	bot = botLogin ()
 	comicStrip = random.randrange (0,3)
 	comicTweet (comicStrip)
 	write (comicStrip)
